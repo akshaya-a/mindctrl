6634e4bc46ad79e82c1267858af2e62eff4de39a
Akshaya Annavajhala <akshaya-a@users.noreply.github.com>
2023-10-30 15:53:53 -0700

    ingress port collision (#2)

--- mlflow-gateway/run.sh
+++ mlflow-gateway/run.sh
@@ -4,4 +4,4 @@ echo "Starting MLflow gateway"
 key=$(python /config.py)
 echo $key
 export OPENAI_API_KEY=$key
-mlflow gateway start --config-path /route-config.yaml --port 8099 --host 0.0.0.0
+mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
--- mlflow-tracking/run.sh
+++ mlflow-tracking/run.sh
@@ -5,4 +5,4 @@ mlflow server \
   --backend-store-uri sqlite:///data/mydb.sqlite \
   --artifacts-destination /share/tracking/mlflow-hass \
   --host 0.0.0.0 \
-  --port 8099
+  --port 5000
