26bc46f9c4d9c1c59daf622c612ee69e662758cc
AK <akshayaa@outlook.com>
2023-10-30 18:11:38 -0700

    point tracking at gateway

--- mlflow-gateway/run.sh
+++ mlflow-gateway/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Starting MLflow gateway"
+
 key=$(python /config.py)
-echo $key
 export OPENAI_API_KEY=$key
-export PYTHONPATH="/"
+
 mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
--- mlflow-tracking/run.sh
+++ mlflow-tracking/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Starting MLflow server"
+
+# TODO: convert this to Supervisor REPO_SLUG format via addon discovery
+export MLFLOW_GATEWAY_URI="http://localhost:5001"
+
 mlflow server \
   --backend-store-uri sqlite:///data/mydb.sqlite \
   --artifacts-destination /share/tracking/mlflow-hass \
