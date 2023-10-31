8d9824fc2220e1807561292179c481d75c430818
AK <akshayaa@outlook.com>
2023-10-30 17:54:17 -0700

    patch psutil

--- mlflow-gateway/run.sh
+++ mlflow-gateway/run.sh
@@ -4,4 +4,5 @@ echo "Starting MLflow gateway"
 key=$(python /config.py)
 echo $key
 export OPENAI_API_KEY=$key
+export PYTHONPATH="/"
 mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
