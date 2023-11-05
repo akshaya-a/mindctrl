a78310612a60ab1898c12212f521928333b74ea7
AK <akshayaa@outlook.com>
2023-11-05 00:09:11 -0700

    add gateway check

--- mlflow-addon/rootfs/usr/bin/run_gateway.sh
+++ mlflow-addon/rootfs/usr/bin/run_gateway.sh
@@ -5,4 +5,4 @@ echo "Starting MLflow gateway"
 export OPENAI_API_KEY="$(bashio::config 'OPENAI_API_KEY')"
 
 # rootfs/route-config.yaml
-mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
+s6-notifyoncheck mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
