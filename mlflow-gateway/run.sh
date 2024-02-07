1fe25b17c0d61ecc2355bf7c51cdb02b3acce670
AK <akshayaa@outlook.com>
2024-02-06 20:21:29 -0800

    remove unused addons and bump config for builder

--- mlflow-gateway/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bashio
-
-echo "Starting MLflow gateway"
-
-export OPENAI_API_KEY="$(bashio::config 'OPENAI_API_KEY')"
-
-mlflow gateway start --config-path /route-config.yaml --port 5001 --host 0.0.0.0
