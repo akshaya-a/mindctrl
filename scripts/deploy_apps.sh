099c59447d41bcac9cc2322e22dcef972fa1be15
AK <akshayaa@outlook.com>
2024-03-23 15:23:35 -0700

    fix deploy script

--- scripts/deploy_apps.sh
+++ scripts/deploy_apps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu
 
-cd ./mindctrl-addon/rootfs/usr/bin
+cd ./mindctrl-addon/rootfs/usr/bin/deploy
 
 echo "using target registry $K3D_REGISTRY_URL"
 
-envsubst < /deployments.yml | kubectl apply -f -
-envsubst < /tracking.yml | kubectl apply -f -
-envsubst < /multiserver.yml | kubectl apply -f -
+envsubst < ./deployments.yml | kubectl apply -f -
+envsubst < ./tracking.yml | kubectl apply -f -
+envsubst < ./multiserver.yml | kubectl apply -f -
