167fdcf7686609652f721d1ed8680ad79ae733ab
AK <akshayaa@outlook.com>
2024-03-23 15:54:50 -0700

    wrong file extension..

--- scripts/deploy_apps.sh
+++ scripts/deploy_apps.sh
@@ -2,11 +2,9 @@
 set -eu
 
 cd ./mindctrl-addon/rootfs/usr/bin/deploy
-pwd
-ls -la
 
 echo "using target registry $K3D_REGISTRY_URL"
 
-envsubst < ./deployments.yml | kubectl apply -f -
-envsubst < ./tracking.yml | kubectl apply -f -
-envsubst < ./multiserver.yml | kubectl apply -f -
+envsubst < deployments.yaml | kubectl apply -f -
+envsubst < tracking.yaml | kubectl apply -f -
+envsubst < multiserver.yaml | kubectl apply -f -
