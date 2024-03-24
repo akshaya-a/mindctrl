268f717383ba5cfdaf789bf57baa64f1363f989a
AK <akshayaa@outlook.com>
2024-03-23 19:38:48 -0700

    use different registry for pull

--- scripts/deploy_apps.sh
+++ scripts/deploy_apps.sh
@@ -4,7 +4,16 @@ set -eu
 cd ./mindctrl-addon/rootfs/usr/bin/deploy
 
 echo "using target registry $K3D_REGISTRY_URL"
+echo "using registry for pull $REGISTRY_NAME:$REGISTRY_PORT"
 
-envsubst < deployments.yaml | kubectl apply -f -
-envsubst < tracking.yaml | kubectl apply -f -
-envsubst < multiserver.yaml | kubectl apply -f -
+envsubst < deployments.yaml > deployments-resolved.yaml
+cat deployments-resolved.yaml
+kubectl apply -f deployments-resolved.yaml
+
+envsubst < tracking.yaml > tracking-resolved.yaml
+cat tracking-resolved.yaml
+kubectl apply -f tracking-resolved.yaml
+
+envsubst < multiserver.yaml > multiserver-resolved.yaml
+cat multiserver-resolved.yaml
+kubectl apply -f multiserver-resolved.yaml
